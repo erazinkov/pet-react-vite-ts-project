29fd47e96aaece0b273ea0f1173ef6629cebf367
erazinkov <egorrazinkov@gmail.com>
2026-05-20 14:05:50 +0300

    small fix

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,5 +1,12 @@
+import { useSelector } from 'react-redux';
+import Heading from '../../components/Heading/Heading';
+import styles from './Cart.module.css';
+import type { RootState } from '../../store/store';
+
 
 export function Cart() {
-	return <>
-    Cart</>;
+	const items = useSelector((state: RootState) => state.cart.items);
+	return (<>
+		<Heading>Корзина</Heading>
+	</>);
 }
